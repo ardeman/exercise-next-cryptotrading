a00b97e0a9930d2db28d35e16ebe28b3ff66f95f
Ardeman <mail@ardeman.com>
2023-08-11 09:25:50 +0700

    Add title

--- src/pages/index.js
+++ src/pages/index.js
@@ -92,6 +92,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <h1 className="uppercase text-lg font-bold">{symbol}</h1>
       <CandlestickChart chartData={chartData} />
       <OrderBook asks={asks} bids={bids} />
     </main>
